5c7fce6074d0027b03275f6ff19f733e5b6923c7
Henry Stanley <henry@henrystanley.com>
2015-07-21 15:32:46 +0100

    app now returns full ENV hash

--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 require "sinatra"
 
 get "/" do
-  current_ip = Socket.ip_address_list.
-    detect { |intf| intf.ipv4_private? }.
-    ip_address
-
-  "ip-address: #{current_ip}"
+  ENV.inspect
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,13 +10,9 @@ RSpec.describe "App" do
 
   context "GET /" do
     it "returns the IP address" do
-      current_ip = Socket.ip_address_list.
-        detect { |intf| intf.ipv4_private? }.
-        ip_address
-
       get "/"
 
-      expect(last_response.body).to eq("ip-address: #{current_ip}")
+      expect(last_response.body).to eq(ENV.inspect)
     end
   end
 end
