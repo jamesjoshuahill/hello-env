72b1a72353d95383ed6e9f7ab959e3f62ad69802
Henry Stanley <henry@henrystanley.com>
2015-07-21 16:08:33 +0100

    add basic logging

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require "sinatra"
 
+configure do
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
 get "/" do
   ENV.inspect
 end
